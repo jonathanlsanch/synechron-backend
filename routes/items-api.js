d5f191884db956da1a0b4fdf7094fd6296a10271
Jonathan Sanchez <jonathanlsanch@gmail.com>
2018-05-20 13:52:50 -0400

    create item working

--- routes/items-api.js
+++ routes/items-api.js
@@ -14,4 +14,30 @@ router.get('/items', (req, res, next) => {
   });
 });
 
+/* CREATE a new Item. */
+router.post('/items', (req, res, next) => {
+    const theItem = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      rating: req.body.rating,
+      seller: req.body.seller,
+      warranty: req.body.warranty,
+    });
+  
+    theItem.save((err) => {
+      if (err) {
+        res.json(err);
+        return;
+      }
+  
+      res.json({
+        message: 'New Item created',
+        id: theItem._id
+      });
+    });
+  });
+
+
+
 module.exports = router;
