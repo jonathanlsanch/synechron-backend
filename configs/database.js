fd9dbaae2d7ac7e3c47a0080663c7e34a1b796f1
Jonathan Sanchez <jonathanlsanch@gmail.com>
2018-05-21 12:35:02 -0400

    configure database

--- configs/database.js
+++ configs/database.js
@@ -1,10 +1,14 @@
 'use strict';
 
+require("dotenv").config();
+
 const mongoose = require('mongoose');
 const dbName = 'synechron-store';
 
 // connect to the database
-mongoose.connect(`mongodb://localhost/${dbName}`);
+// mongoose.connect(`mongodb://localhost/${dbName}`);
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
